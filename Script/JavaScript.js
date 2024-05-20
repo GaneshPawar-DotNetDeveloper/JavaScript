28e564130350e94cbcc8f5c34f2933fa78bd7547
Ganesh Pawar <165685426+GaneshPawar-DotNetDeveloper@users.noreply.github.com>
2024-05-20 20:41:19 +0530

    Add files via upload

---
+++ Script/JavaScript.js
@@ -0,0 +1,5 @@
+﻿function btnclick() {
+    alert('we call external javaScript file');
+}
+/*document.getElementById('btn1').onclick = alert('hello ganesh pawar');*/
+document.getElementById('btn1').onclick = btnclick;
